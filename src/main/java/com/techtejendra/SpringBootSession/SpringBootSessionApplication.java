f227c953940d179e34234a6b64429bcb0497f21c
TEJENDRA RANA <tejendra.rana@in.ibm.com>
2020-07-26 09:39:43 +0530

    first commit

---
+++ src/main/java/com/techtejendra/SpringBootSession/SpringBootSessionApplication.java
@@ -0,0 +1,13 @@
+package com.techtejendra.SpringBootSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtejendra/SpringBootSession/SpringBootSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtejendra.SpringBootSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
